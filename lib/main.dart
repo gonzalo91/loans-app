b74cd4a2408af0895f545a08eb2a2bfb7ad2f008
Gonzalo Cavazos <gonzalo.adriancr@gmail.com>
2022-03-02 22:40:57 -0600

    Main refactor

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:loans_flutter/pages/loans_page.dart';
+import 'package:loans_flutter/pages/login_page.dart';
+import 'package:loans_flutter/pages/orders_page.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -13,248 +16,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  bool _obscureText = true;
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-IconData _iconVisible = Icons.visibility_off;
-   void _toggleObscureText() {
-    setState(() {
-      _obscureText = !_obscureText;
-      if (_obscureText == true) {
-        _iconVisible = Icons.visibility_off;
-      } else {
-        _iconVisible = Icons.visibility;
-      }
-    });
-  }
-
-  Color _backgroundColor = Color(0xFFE43F3F);
-  Color _underlineColor = Color(0xFFCCCCCC);
-  Color _buttonColor = Color(0xFFCC1D1D);
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-        backgroundColor: _backgroundColor,
-        body: AnnotatedRegion<SystemUiOverlayStyle>(
-          value: Platform.isIOS?SystemUiOverlayStyle.light:SystemUiOverlayStyle(
-            statusBarIconBrightness: Brightness.light
-          ),
-          child: ListView(
-            padding: EdgeInsets.fromLTRB(32, 72, 32, 24),
-            children: [
-              Container(
-                child: Image.asset('assets/images/logo_dark.png', height: 120),
-              ),
-              SizedBox(
-                height: 32,
-              ),
-              TextField(
-                keyboardType: TextInputType.emailAddress,
-                style: TextStyle(color: Colors.white),
-                cursorColor: Colors.white,
-                decoration: InputDecoration(
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white, width: 2.0)),
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: _underlineColor),
-                  ),
-                  labelText: 'Email',
-                  labelStyle: TextStyle(color: Colors.white)),
-              ),
-              SizedBox(
-                height: 24,
-              ),
-              TextField(
-                obscureText: _obscureText,
-                style: TextStyle(color: Colors.white),
-                cursorColor: Colors.white,
-                decoration: InputDecoration(
-                  focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.white, width: 2.0)),
-                  enabledBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: _underlineColor),
-                  ),
-                  labelText: 'Password',
-                  labelStyle: TextStyle(color: Colors.white),
-                  suffixIcon: IconButton(
-                      icon: Icon(_iconVisible, color: Colors.white, size: 20),
-                      onPressed: () {
-                        _toggleObscureText();
-                      }),
-                ),
-              ),
-              SizedBox(
-                height: 24,
-              ),
-              GestureDetector(
-                onTap: () {
-                  
-                },
-                child: Text('Forgot Password?',
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 13
-                    ),
-                    textAlign: TextAlign.right
-                ),
-              ),
-              SizedBox(
-                height: 40,
-              ),
-              ElevatedButton(
-                  style: ButtonStyle(
-                    backgroundColor: MaterialStateProperty.resolveWith<Color>(
-                          (Set<MaterialState> states) => _buttonColor,
-                    ),
-                    overlayColor: MaterialStateProperty.all(Colors.transparent),
-                    shape: MaterialStateProperty.all(
-                        RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(3.0),
-                        )
-                    ),
-                  ),
-                  onPressed: () {
-                    
-                  },
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(vertical: 12),
-                    child: Text(
-                      'LOGIN',
-                      style: TextStyle(
-                          fontSize: 16,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white),
-                      textAlign: TextAlign.center,
-                    ),
-                  )
-              ),
-              SizedBox(
-                height: 32,
-              ),
-              Center(
-                child: Text('Sign in with', style: TextStyle(
-                    fontSize: 15, color: Colors.white
-                )),
-              ),
-              SizedBox(
-                height: 32,
-              ),
-              Container(
-                margin: EdgeInsets.symmetric(horizontal: 24),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: <Widget>[
-                    GestureDetector(
-                      onTap: (){
-                        
-                      },
-                      child: Container(
-                        padding: EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.all(
-                              Radius.circular(4)
-                          ),
-                        ),
-                        child: Image(
-                          image: AssetImage('assets/images/google.png'),
-                          width: 24,
-                        ),
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: (){
-                        
-                      },
-                      child: Image(
-                        image: AssetImage('assets/images/facebook.png'),
-                        width: 40,
-                        color: Colors.white
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: (){
-                        
-                      },
-                      child: Image(
-                        image: AssetImage('assets/images/twitter.png'),
-                        width: 40,
-                        color: Colors.white
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              SizedBox(
-                height: 60,
-              ),
-              Center(
-                child: GestureDetector(
-                  onTap: () {
-                    
-                  },
-                  child: Text('No account yet? Create one', style: TextStyle(
-                      fontSize: 15, color: Colors.white
-                  )),
-                ),
-              )
-            ],
-          ),
-        )
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const LoginPage(),
+        '/loans-page': (context) => const LoansPage(),
+        '/orders-page': (context) => const OrdersPage(),
+      },
     );
   }
 }
